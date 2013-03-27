bd4ddf6ece55726f2da00e49d51a4a75c279a5d9
Thomas Vogt <tuxor1337@web.de>
2013-03-27 15:35:48 +0100

    Fix str-int-concatenation error

--- springerdl/pdftoc.py
+++ springerdl/pdftoc.py
@@ -33,16 +33,22 @@ def _getDestinationPageNumbers(pdf):
                 _num_pages.append(1)
         return _result
 
-    outline_page_ids = _setup_outline_page_ids(pdf.getOutlines())
+    try:
+        outline_page_ids = _setup_outline_page_ids(pdf.getOutlines())
+    except ValueError:
+        "This should not happen, but does e.g. with 978-1-4419-6757-2!"
+        return None
     page_id_to_page_numbers = _setup_page_id_to_num()
 
     result = []
     for (_, title), (page_idnum,lvl) in outline_page_ids.iteritems():
-        result.append([title,page_id_to_page_numbers.get(page_idnum, '???'),lvl])
+        if page_id_to_page_numbers.get(page_idnum) != None:
+            result.append([title,page_id_to_page_numbers.get(page_idnum),lvl])
     return result
    
 def getTocFromPdf(pdf,shift=0,default=None,baselvl=0,child_cnt=0):
     ch = _getDestinationPageNumbers(pdf)
+    if ch == None: return []
     ch.sort(key=lambda x: x[1:3])
     ch = [[decodeForSure(x[0]),x[1]+shift,x[2]+baselvl] for x in ch]
     for i in range(len(ch)):
