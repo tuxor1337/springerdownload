5bff80430686242f2fcf0c13beb2e1922d75efe3
Manuel <m@m.m>
2013-06-27 11:34:09 +0200

    Fix executable detection on windows. Previously the path separator was not portable ( : vs ; on windows) and executable extensions were ignored.
    Solution copied from http://stackoverflow.com/questions/377017/test-if-executable-exists-in-python

--- springerdl/const.py
+++ springerdl/const.py
@@ -1,4 +1,3 @@
-
 import os
 
 SPRINGER_URL = "http://link.springer.com"
@@ -6,13 +5,28 @@ SPR_IMG_URL  = "http://images.springer.com"
 DOWNLOAD_CHUNK_SIZE = 65536
 USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:14.0) Gecko/20100101 Firefox/14.0.1'
 
-BINPATH = { "gs": None, "pdftk": None, "convert": None}
-for p in reversed(os.getenv("PATH").split(":")):
-    for f in ["gs","convert","pdftk"]:
-        candidate = os.path.join(p,f)
-        if os.path.isfile(candidate):
-            BINPATH[f] = candidate
+def which(program):
+    def is_exe(fpath):
+        return os.path.exists(fpath) and os.access(fpath, os.X_OK)
 
-GS_BIN = BINPATH['gs']
-IM_BIN = BINPATH['convert']
-PDFTK_BIN = BINPATH['pdftk']
+    def ext_candidates(fpath):
+        yield fpath
+        for ext in os.environ.get("PATHEXT", "").split(os.pathsep):
+            yield fpath + ext
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            exe_file = os.path.join(path, program)
+            for candidate in ext_candidates(exe_file):
+                if is_exe(candidate):
+                    return candidate
+
+    return None
+
+GS_BIN = which('gs')
+IM_BIN = which('convert')
+PDFTK_BIN = which('pdftk')
